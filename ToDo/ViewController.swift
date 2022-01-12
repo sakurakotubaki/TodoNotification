95026c36fd5c76f51bba3c3038ae62ef03ac3aeb
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-01-12 23:03:45 +0900

    TableViewを使ってみようが終わった

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") //cellはStep2で指定した文字列
+        cell?.textLabel?.text = "左に表示されるテキスト"
+        cell?.detailTextLabel?.text = "右に表示されるテキスト"
+        return cell!
+    }
+    
 }
 
