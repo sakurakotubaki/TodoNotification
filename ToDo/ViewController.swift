aaa16883b8a8d3099c776e139c0c9e1c102a8920
Flutter_SakuSaku13:44 <gitonly543@gmail.com>
2022-01-12 23:59:28 +0900

    tableViewに保存したデータを表示できた

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -6,22 +6,39 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
+    // Realmからデータを読み込む
+    var itemList:Results<TodoItem>!
+    // レルムのインスタンスが必要
+    let realm = try! Realm()
+    // データベースに変更があった場合に再読み込み
+    var token:NotificationToken!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // 日付順に並べ替え
+        itemList = realm.objects(TodoItem.self).sorted(byKeyPath: "date")
+        token = realm.observe { notification, realm in
+            // 変更があった場合にここが実行される。,@IBOutlet weak var tableView: UITableView!が必要
+            self.tableView.reloadData()
+        }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return itemList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") //cellはStep2で指定した文字列
-        cell?.textLabel?.text = "左に表示されるテキスト"
-        cell?.detailTextLabel?.text = "右に表示されるテキスト"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = itemList[indexPath.row].title
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd HH:mm"
+        cell?.detailTextLabel?.text = formatter.string(from: itemList[indexPath.row].date)
         return cell!
     }
     
